6b33abeee86ea586b5a1972347fbabc787287b5a
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-03 11:03:17 +0530

    edited texts and added meta data

--- src/utils/text-constants.ts
+++ src/utils/text-constants.ts
@@ -7,5 +7,5 @@ export const homeHeroTexts = {
 export const aboutTexts = {
     title: 'Full Stack developer',
     name: 'hi, my name is Balaji Ramasamy',
-    description: `I develop full stack web and mobile applications. I am passionate about developing applications. I like to develop to application that deals with real world problems and makes the client's work way easier. This blog is tell a bit about myself to the world. I have plans to write on blogs to document the challenges I face and found intresting during my dev-life.`
+    description: `I develop full stack web and mobile applications. I am passionate about developing applications. I like to develop to application that deals with real world problems and makes the client's work way more easier. This site is to tell a bit about myself to the world. I have plans to write on blog posts to document the challenges I face and find intresting in my dev-life.`
 }
