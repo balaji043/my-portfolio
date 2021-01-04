e57fe2d77efb6769f16cca0898f1839dc23fa963
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-04 22:14:36 +0530

    removed export of deleted components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,16 +1,7 @@
-import A from "./A";
-import Icon, { IconType } from "./Icon";
-import Layout from "./Layout";
+import NavLayout from "./NavLayout";
 import PageTitle from "./PageTitle";
-import SocialMediaIcon from "./SocialMediaIcon";
-import TopNavBar from "./TopNavBar";
 
 export {
-    Layout,
-    TopNavBar,
-    PageTitle,
-    SocialMediaIcon,
-    IconType,
-    A,
-    Icon
+    NavLayout,
+    PageTitle
 }
