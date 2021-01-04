51f4915a23f8093cc10b099372b351c851c0958f
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-04 22:15:57 +0530

    removed deleted imports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,9 @@
 import { getCustomClassNames } from "./components";
-import { PageItem, pages, getPathTitle } from "./pages";
-import { socialMedias } from "./social-media";
+import { PageItem } from "./pages";
 import { homeHeroTexts, aboutTexts } from "./text-constants";
 
 export {
     getCustomClassNames,
-    getPathTitle,
-    pages,
-    socialMedias,
     homeHeroTexts,
     aboutTexts,
 }
