abcd7d043e9f25897e7aa39cf54f5766632dc941
balaji043 <cubanhog.royalgamma@gmail.com>
2021-01-05 15:37:59 +0530

    added developer activity icons

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,8 +1,10 @@
 import AppLogo from "assets/svgs/Logo";
 import { FaBars, FaTimes } from "react-icons/fa";
+import DeveloperActivity from 'assets/svgs/developer_activity.svg'
 
 export {
     FaBars as MenuBarIcon,
     FaTimes as CloseSideBarIcon,
     AppLogo,
+    DeveloperActivity
 }
